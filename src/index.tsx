497ac70682b7030191d51b0a68c0e742a550ef5e
David Silva <davidlsilva.sp@gmail.com>
2022-06-27 09:30:34 -0300

    Inicio conexão MirageJs

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-
+import { createServer } from 'miragejs';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+    this.get('/transactions',()=>{
+      return [
+        {
+          id:1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createAt: new Date()
+        }
+      ]
+    })
+  }
+})
+
+
 root.render(
   <React.StrictMode>
     <App />
