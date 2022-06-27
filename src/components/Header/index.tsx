53cf1b6fdafb1b6a8fe661fb9824ae861c3d6c78
David Silva <david.silva@seriedesign.com.br>
2022-06-27 15:59:31 -0300

    Modal + estilização

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,25 @@
-import logoImg from '../../assets/logo.svg';
-import { Container,Content } from './styles';
+import { useState } from "react";
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
 
-export function Header() {
-  return(
-  <Container>
-    <Content>
-    <img src={logoImg} alt="dt money" />
-    <button>
-        Nova transação
-    </button>
-    </Content>
-  </Container>
+
+interface HeaderProps {
+  onOpenNewTransactionModal: ()=>void;
+}
+
+
+export function Header({onOpenNewTransactionModal}:HeaderProps) {
+
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+
+      </Content>
+    </Container>
   );
 }
